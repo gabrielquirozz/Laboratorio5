201a3d02718536bc6cab43a0ad2d1cc46029a2b3
Gabriel Quiroz <46507325+gabrielquirozz@users.noreply.github.com>
2020-02-24 02:03:13 -0600

    t

--- app/src/main/java/com/example/lab5/TitleFragment.kt
+++ app/src/main/java/com/example/lab5/TitleFragment.kt
@@ -5,11 +5,9 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.NavigationUI
 import com.example.lab5.databinding.FragmentTitleBinding
 
 /**
@@ -38,29 +36,29 @@ class TitleFragment : Fragment() {
         binding.floatingActionButton.setOnClickListener{
             it.findNavController().navigate(R.id.action_titleFragment_to_optionFragment)
         }
-        return binding.root
-    }
-
-    fun onCreatePreguntas(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(
-            inflater,
-            R.layout.fragment_title,
-            container,
-            false
-        )
-        viewModel = activity?.run {
-            ViewModelProviders.of(this).get(MainViewModel::class.java)
-        } ?: throw Exception("HOOLA")
-        /*viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)*/
         binding.button2.setOnClickListener{
             it.findNavController().navigate(R.id.action_titleFragment_to_agregarPregunta)
         }
         return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_principal, menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item!!.itemId)
+        {
+            R.id.titleFragment ->  {
+            findNavController().navigate(R.id.action_titleFragment_to_acercaDe)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
 
 
 
